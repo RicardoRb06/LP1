db6679b988939c3756f13136b8128b25d058c3d9
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-04-16 09:59:22 -0300

    create VooInternacional class

---
+++ src/main/java/Lista02_HerancaPolimorfismo/Q4/VooInternacional.java
@@ -0,0 +1,19 @@
+package Lista02_HerancaPolimorfismo.Q4;
+
+public class VooInternacional {
+
+    private final double fatorPreco = 1500;
+
+    public VooInternacional(String origem, String destino, double distancia) {
+        super(origem, destino, distancia);
+    }
+
+    public VooInternacional(double distancia) {
+        super(distancia);
+    }
+
+    @Override
+    public double calcularPreco() {
+        return this.fatorPreco * this * distancia;
+    }
+}
