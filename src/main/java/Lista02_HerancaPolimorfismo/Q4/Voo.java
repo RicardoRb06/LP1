be223493593d8570fbce1381ac69ea5181042ff3
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-04-16 09:51:19 -0300

    create voo class

---
+++ src/main/java/Lista02_HerancaPolimorfismo/Q4/Voo.java
@@ -0,0 +1,11 @@
+package Lista02_HerancaPolimorfismo.Q4;
+
+public abstract class Voo {
+
+    abstract String origem;
+    abstract String destino;
+    abstract double distancia;
+
+    public abstract double calcularPreco();
+
+}
