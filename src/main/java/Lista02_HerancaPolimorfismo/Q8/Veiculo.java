c2d3df855834bf7e7c6e3021af7274e93ec1d01b
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-04-27 22:26:41 -0300

    preco now is float

--- src/main/java/Lista02_HerancaPolimorfismo/Q8/Veiculo.java
+++ src/main/java/Lista02_HerancaPolimorfismo/Q8/Veiculo.java
@@ -5,14 +5,14 @@ public abstract class Veiculo {
     protected String modelo;
     protected float preco;
 
-    public void setPreco(int preco) {
+    public void setPreco(float preco) {
         if (preco < 0){
             throw new IllegalArgumentException("Preço não pode ser negativo");
         }
         this.preco = preco;
     }
 
-    public Veiculo(String marca, String modelo, int preco) {
+    public Veiculo(String marca, String modelo, float preco) {
         this.marca = marca;
         this.modelo = modelo;
         this.setPreco(preco);
